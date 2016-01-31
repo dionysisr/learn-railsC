eaa7b5fa16866e5cff331ba51d863273b011aa2d
dionysisr <dionysisr@gmail.com>
2016-01-31 08:17:51 +0000

    update application layout (including flash msgs) p.204 learn-rails2

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,6 +2,9 @@ class VisitorsController < ApplicationController
     
     def new
         @owner = Owner.new
+        flash[:notice] = "I am a notice!"
+        flash[:alert] = "I am an ALERT!"
+        flash[:flash] = "I am a general purpose flash message..."
     end
     
 end
